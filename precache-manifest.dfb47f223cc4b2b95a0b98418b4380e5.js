c88892ce42df6cd6d430941614d0dc934065f76b
Iris Tsai <pokailin17@gmail.com>
2019-11-21 11:05:06 +0800

    Updates

---
+++ precache-manifest.dfb47f223cc4b2b95a0b98418b4380e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "537cd2831a9ea9cbd2a7b2d9e0b2b4a7",
+    "url": "/robostore/index.html"
+  },
+  {
+    "revision": "48781ede84185ee087df",
+    "url": "/robostore/static/css/main.4fb4802c.chunk.css"
+  },
+  {
+    "revision": "275cb98ef5559903d979",
+    "url": "/robostore/static/js/2.ffea93fd.chunk.js"
+  },
+  {
+    "revision": "48781ede84185ee087df",
+    "url": "/robostore/static/js/main.506ba181.chunk.js"
+  },
+  {
+    "revision": "bfcf5c1d52fa795d140a",
+    "url": "/robostore/static/js/runtime-main.b94c35c1.js"
+  },
+  {
+    "revision": "75ee1ceb731fbf2a3411e536b92ffd61",
+    "url": "/robostore/static/media/robot.75ee1ceb.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/robostore/precache-manifest.dfb47f223cc4b2b95a0b98418b4380e5.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robostore/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
